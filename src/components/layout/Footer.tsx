9a61cbd0b2d75168b396413def247a5c7db67d9e
Dmitry Ginzburg <33052194+Ultraivanov@users.noreply.github.com>
2026-04-05 01:11:59 +0300

    polish(home): align typography and footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Container from "./Container";
 import styles from "./layout.module.css";
 
@@ -5,15 +6,20 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <Container className={styles.footerInner}>
-        <div className={styles.footerMeta}>Product Designer</div>
-        <div className={styles.footerMeta}>Based in Tel Aviv</div>
-        <div className={styles.footerLinks}>
-          <a className={styles.footerLink} href="/privacy">
-            Privacy Policy
-          </a>
-          <a className={styles.footerLink} href="/terms">
-            Terms of Use
-          </a>
+        <p className={styles.footerMeta}>Based in Tel Aviv</p>
+        <a className={styles.footerLink} href="/privacy">
+          Privacy Policy
+        </a>
+        <a className={styles.footerLink} href="/terms">
+          Terms of Use
+        </a>
+        <div className={styles.footerMark}>
+          <Image
+            src="/home/footer-mark.png"
+            alt="Fundrising design"
+            width={97}
+            height={36}
+          />
         </div>
       </Container>
     </footer>

4de78ccbcf9e154fe8d6315e7a4e6853f46120bb
Dmitry Ginzburg <33052194+Ultraivanov@users.noreply.github.com>
2026-04-04 15:18:19 +0300

    fix: render keystatic ui as client

--- src/app/keystatic/[[...slug]]/page.tsx
+++ src/app/keystatic/[[...slug]]/page.tsx
@@ -1,4 +1,16 @@
-import { makePage } from "@keystatic/next/ui/app";
+"use client";
+
+import { Keystatic } from "@keystatic/core/ui";
 import keystaticConfig from "../../../../keystatic.config";
 
-export default makePage(keystaticConfig);
+export default function KeystaticPage() {
+  return (
+    <Keystatic
+      config={keystaticConfig}
+      appSlug={{
+        envName: "NEXT_PUBLIC_KEYSTATIC_GITHUB_APP_SLUG",
+        value: process.env.NEXT_PUBLIC_KEYSTATIC_GITHUB_APP_SLUG,
+      }}
+    />
+  );
+}

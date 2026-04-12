f2282109a929d6a1d5cdf86f5dc91144c33d6ef3
Dmitry Ginzburg <33052194+Ultraivanov@users.noreply.github.com>
2026-04-12 23:43:47 +0300

    fix: disable cache for admin page

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,23 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: "/admin/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+      {
         source: "/:path*",
         headers: [
           {
